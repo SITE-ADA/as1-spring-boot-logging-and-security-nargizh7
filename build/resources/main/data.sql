21fda89e57f028404a3416fbe7dea630342bcb27
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-04-28 21:28:59 +0400

    working on task 1

---
+++ build/resources/main/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO MOVIES(name, country, wins) VALUES('Inception', 'USA', 4);
+INSERT INTO MOVIES(name, country, wins) VALUES('The Dark Knight', 'USA', 2);
+INSERT INTO MOVIES(name, country, wins) VALUES('Interstellar', 'USA', 1);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('Pulp Fiction', 'USA', 1);
+INSERT INTO MOVIES(name, country, wins) VALUES('Django Unchained', 'USA', 2);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('Jurassic Park', 'USA', 3);
+INSERT INTO MOVIES(name, country, wins) VALUES('Schindlers List', 'USA', 7);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('The Departed', 'USA', 4);
+INSERT INTO MOVIES(name, country, wins) VALUES('Goodfellas', 'USA', 1);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('Psycho', 'USA', 0);
+INSERT INTO MOVIES(name, country, wins) VALUES('Rear Window', 'USA', 0);
